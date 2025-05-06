c2470c31d643f98c00488be80914dbc42c305cdd
Warrior Rockk <warrior@Warrior-MBAM2.local>
2025-05-06 21:30:04 +0200

    using game.h

--- src/main.c
+++ src/main.c
@@ -6,25 +6,10 @@
 #include "allegro.h"
 
 #include "globals.h"
+#include "game.h"
 #include "entity.h"
 #include "collisions.h"
 #include "scroll.h"
-
-//SMS resolution: 256x192 (testing 256x208: extra sms Y tile to center screen)
-#define SCREEN_X        320
-#define SCREEN_Y        240
-
-#define GAME_W          256
-#define GAME_H          208
-#define GAME_X          (SCREEN_W>>1) - (GAME_W>>1)
-#define GAME_Y          (SCREEN_H>>1) - (GAME_H>>1)
-
-#define NUM_TILES       3
-#define TILE_W          16
-#define TILE_H          16
-
-#define MAP_TILE_W      (GAME_W / TILE_W) * 2
-#define MAP_TILE_H      (GAME_H / TILE_H) * 1
 
 struct player  
 {
