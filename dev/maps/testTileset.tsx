0eb7e8e6fe4a426517b1072b6348d8d0c8885c31
Warrior Rockk <warrior@Warrior-MBAM2.local>
2025-05-17 20:02:14 +0200

    start working on tiled maps

---
+++ dev/maps/testTileset.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="testTileset" tilewidth="16" tileheight="16" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="code/dos/projects/dos-platform-game/res/tiles/001.bmp" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="code/dos/projects/dos-platform-game/res/tiles/002.bmp" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="code/dos/projects/dos-platform-game/res/tiles/003.bmp" width="16" height="16"/>
+ </tile>
+</tileset>
