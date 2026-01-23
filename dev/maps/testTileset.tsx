e16c79c53301ca2d479fdfb8dea72b4ae47349b2
Warrior Rockk <warrior@Warrior-MBAM2.local>
2026-01-23 15:35:27 +0100

    working on new player character and animations

--- dev/maps/testTileset.tsx
+++ dev/maps/testTileset.tsx
@@ -2,12 +2,15 @@
 <tileset version="1.10" tiledversion="1.11.2" name="testTileset" tilewidth="16" tileheight="16" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image source="code/dos/projects/dos-platform-game/res/tiles/001.bmp" width="16" height="16"/>
+  <properties>
+   <property name="code" type="int" value="10"/>
+  </properties>
+  <image source="../../res/tiles/001.bmp" width="16" height="16"/>
  </tile>
  <tile id="1">
-  <image source="code/dos/projects/dos-platform-game/res/tiles/002.bmp" width="16" height="16"/>
+  <image source="../../res/tiles/002.bmp" width="16" height="16"/>
  </tile>
  <tile id="2">
-  <image source="code/dos/projects/dos-platform-game/res/tiles/003.bmp" width="16" height="16"/>
+  <image source="../../res/tiles/003.bmp" width="16" height="16"/>
  </tile>
 </tileset>
