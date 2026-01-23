dc3bd619a247db05f79f15b08fbb81185d38b022
Warrior Rockk <warrior@Warrior-MBAM2.local>
2026-01-23 17:30:31 +0100

    tweak player friction

--- src/player.c
+++ src/player.c
@@ -37,7 +37,7 @@ void player_init(tEntity *player)
 void player_update(tEntity *player)
 {
     fixed accel_x = ftofix(0.06);
-    fixed friction = ftofix(0.86);       //more friction, more sloppy (0.94-0.96 is like ice)
+    fixed friction = ftofix(0.92);       //more friction, more sloppy (0.94-0.96 is like ice) 0.86 testing
     fixed air_friction = ftofix(0.6);   //less than floor friction
     fixed gravity = ftofix(0.2);
     fixed accel_y = ftofix(4.0);        //jump acceleration
