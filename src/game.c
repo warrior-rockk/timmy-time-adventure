237426c473e9eff5ad1266c305506a67cfae2433
warrior-rockk <warrior.rockk@gmail.com>
2026-05-13 11:03:51 +0200

    set working level on debugmode

--- src/game.c
+++ src/game.c
@@ -825,7 +825,11 @@ void game_init()
     
     //initialize game flags
     game.prevState      = E_GAME_ST_LOAD_LEVEL;
-    game.actualLevel    = 0;
+    #ifdef DEBUGMODE
+        game.actualLevel    = E_GAME_LEVEL_WEST;
+    #else
+        game.actualLevel    = 0;
+    #endif
     game.lives          = GAME_INI_LIVES;
     game.life           = GAME_INI_LIFE;
     game.score          = 0;
