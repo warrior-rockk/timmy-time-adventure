5a6dd5beaacfbeb5b34e8f6e2ae07a8b84dbd634
warrior-rockk <warrior.rockk@gmail.com>
2026-06-09 08:53:37 +0200

    quicksand big

--- src/objects.c
+++ src/objects.c
@@ -223,7 +223,7 @@ void object_create(tEntity *entity)
             entity->properties = E_ENT_PROP_NO_PICKABLE | E_ENT_PROP_NO_BREAKABLE | E_ENT_PROP_AUTO_DESTROY;            
         break;
         case E_QUICKSAND_OBJECT_TYPE:            
-            entity->size = (tVector){96, 32};                                     
+            entity->size = (tVector){128, 32};                                     
             entity->properties =  E_ENT_PROP_NO_PICKABLE | E_ENT_PROP_NO_BREAKABLE;            
             entity->axis = E_ENT_AXIS_DOWN;
             collision_create_entity_points(entity);            
