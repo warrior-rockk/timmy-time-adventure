fb517a6ac546a12a3bcaca2e3dc3a5c92ceaedb3
Warrior Rockk <warrior@Warrior-MBAM2.local>
2026-07-09 21:15:39 +0200

    remove enum E_ENT_DIR casting

--- src/objects.c
+++ src/objects.c
@@ -1452,7 +1452,7 @@ void object_platform_update(tEntity *this, tSolidObjectLocalData *local)
             collision_apply_dir(this, colDir, E_COLLISION_NO_BOUNCE);       
             
             //change direction on collision
-            if (this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_LEFT || this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_RIGHT)
+            if (this->dir == E_PLATFORM_DIR_LEFT || this->dir == E_PLATFORM_DIR_RIGHT)
             {            
                 //change direction if horizontal collision
                 if (colDir == E_COLLISION_DIR_RIGHT || colDir == E_COLLISION_DIR_LEFT)
@@ -1461,12 +1461,12 @@ void object_platform_update(tEntity *this, tSolidObjectLocalData *local)
                     break;
                 }
             }
-            if (this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_DOWN || this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_UP)
+            if (this->dir == E_PLATFORM_DIR_DOWN || this->dir == E_PLATFORM_DIR_UP)
             {            
                 //change direction if vertical collision
                 if (colDir == E_COLLISION_DIR_UP || colDir == E_COLLISION_DIR_DOWN)
                 {
-                    this->dir = this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_DOWN ? (enum E_ENTITY_DIR)E_PLATFORM_DIR_UP : (enum E_ENTITY_DIR)E_PLATFORM_DIR_DOWN;
+                    this->dir = this->dir == E_PLATFORM_DIR_DOWN ? E_PLATFORM_DIR_UP : E_PLATFORM_DIR_DOWN;
                     break;
                 }
             }                    
@@ -1493,22 +1493,22 @@ void object_platform_update(tEntity *this, tSolidObjectLocalData *local)
             if (CHECK_FLAG(this->spare,E_PLATFORM_TYPE_MOVE_PATROL) && colDir == E_COLLISION_DIR_NONE)
             {
                 //horizontal patrol
-                if (this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_LEFT || this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_RIGHT)
+                if (this->dir == E_PLATFORM_DIR_LEFT || this->dir == E_PLATFORM_DIR_RIGHT)
                 {
                     if ((this->dir && this->pos.x > (this->initPos.x + PLATFORM_MOVE_TILES_X)) || (!this->dir && this->pos.x < (this->initPos.x - PLATFORM_MOVE_TILES_X)))
                         this->dir = !this->dir;
                 }
                 //vertical patrol
-                if (this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_DOWN || this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_UP)
+                if (this->dir == E_PLATFORM_DIR_DOWN || this->dir == E_PLATFORM_DIR_UP)
                 {
-                    if ((this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_DOWN && this->pos.y > (this->initPos.y + PLATFORM_MOVE_TILES_Y)) || (this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_UP && this->pos.y < (this->initPos.y - PLATFORM_MOVE_TILES_Y)))
-                        this->dir = this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_DOWN ? (enum E_ENTITY_DIR)E_PLATFORM_DIR_UP : (enum E_ENTITY_DIR)E_PLATFORM_DIR_DOWN;
+                    if ((this->dir == E_PLATFORM_DIR_DOWN && this->pos.y > (this->initPos.y + PLATFORM_MOVE_TILES_Y)) || (this->dir == E_PLATFORM_DIR_UP && this->pos.y < (this->initPos.y - PLATFORM_MOVE_TILES_Y)))
+                        this->dir = this->dir == E_PLATFORM_DIR_DOWN ? E_PLATFORM_DIR_UP : E_PLATFORM_DIR_DOWN;
                 }
             }
 
             //apply linear velocity
             //horizontal
-            if (this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_LEFT || this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_RIGHT)
+            if (this->dir == E_PLATFORM_DIR_LEFT || this->dir == E_PLATFORM_DIR_RIGHT)
             {
                 this->fixVel.x = this->dir ? ftofix(PLATFORM_VELOCITY) : -ftofix(PLATFORM_VELOCITY);
                 //calculate next integer position (entity update do this)
@@ -1518,9 +1518,9 @@ void object_platform_update(tEntity *this, tSolidObjectLocalData *local)
                     entity_get(entity_get_player_id())->fixPos.x += itofix((nextPos - this->pos.x));
             }
             //vertical
-            if (this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_DOWN || this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_UP)
+            if (this->dir == E_PLATFORM_DIR_DOWN || this->dir == E_PLATFORM_DIR_UP)
             {
-                this->fixVel.y = this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_DOWN ? ftofix(PLATFORM_VELOCITY) : -ftofix(PLATFORM_VELOCITY);
+                this->fixVel.y = this->dir == E_PLATFORM_DIR_DOWN ? ftofix(PLATFORM_VELOCITY) : -ftofix(PLATFORM_VELOCITY);
                 //calculate next integer position (entity update do this)
                 nextPos = fixtoi(this->fixPos.y + fixmul(this->fixVel.y, ftofix(deltaTime)));
                 //adds to player x position the integer part of platform delta movement
@@ -1724,13 +1724,13 @@ void object_path_platform_update(tEntity *this, tPathPlatformLocalData *local)
                     local->pathPos.y = checkEntity->pos.y;
                     //check point direction (don't allow diagonal movement)
                     if (this->pos.x > checkEntity->pos.x) 
-                        this->dir = (enum E_ENTITY_DIR)E_PLATFORM_DIR_LEFT;
+                        this->dir = E_PLATFORM_DIR_LEFT;
                     else if (this->pos.x < checkEntity->pos.x) 
-                        this->dir = (enum E_ENTITY_DIR)E_PLATFORM_DIR_RIGHT;
+                        this->dir = E_PLATFORM_DIR_RIGHT;
                     else if (this->pos.y < checkEntity->pos.y) 
-                        this->dir = (enum E_ENTITY_DIR)E_PLATFORM_DIR_DOWN;
+                        this->dir = E_PLATFORM_DIR_DOWN;
                     else if (this->pos.y > checkEntity->pos.y) 
-                        this->dir = (enum E_ENTITY_DIR)E_PLATFORM_DIR_UP;
+                        this->dir = E_PLATFORM_DIR_UP;
                 }
             }
             this->state++;
@@ -1743,7 +1743,7 @@ void object_path_platform_update(tEntity *this, tPathPlatformLocalData *local)
             int16_t nextPos;    
             
             //horizontal
-            if (this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_LEFT || this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_RIGHT)
+            if (this->dir == E_PLATFORM_DIR_LEFT || this->dir == E_PLATFORM_DIR_RIGHT)
             {
                 //apply linear velocity
                 this->fixVel.x = this->dir ? ftofix(PLATFORM_VELOCITY) : -ftofix(PLATFORM_VELOCITY);
@@ -1755,10 +1755,10 @@ void object_path_platform_update(tEntity *this, tPathPlatformLocalData *local)
                     entity_get(entity_get_player_id())->fixPos.x += itofix((nextPos - this->pos.x));
             }
             //vertical
-            if (this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_DOWN || this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_UP)
+            if (this->dir == E_PLATFORM_DIR_DOWN || this->dir == E_PLATFORM_DIR_UP)
             {
                 //apply linear velocity
-                this->fixVel.y = this->dir == (enum E_ENTITY_DIR)E_PLATFORM_DIR_DOWN ? ftofix(PLATFORM_VELOCITY) : -ftofix(PLATFORM_VELOCITY);
+                this->fixVel.y = this->dir == E_PLATFORM_DIR_DOWN ? ftofix(PLATFORM_VELOCITY) : -ftofix(PLATFORM_VELOCITY);
                 
                 //calculate next integer position (entity update do this)
                 nextPos = fixtoi(this->fixPos.y + fixmul(this->fixVel.y, ftofix(deltaTime)));
@@ -1788,7 +1788,7 @@ void object_path_platform_update(tEntity *this, tPathPlatformLocalData *local)
                         this->state = E_PLATFORM_ST_GET_POINT;
                     }
                 break;
-                case (enum E_ENTITY_DIR)E_PLATFORM_DIR_DOWN:
+                case E_PLATFORM_DIR_DOWN:
                     if (this->pos.y >= local->pathPos.y)
                     {
                         this->fixPos.y = itofix(local->pathPos.y);
@@ -1797,7 +1797,7 @@ void object_path_platform_update(tEntity *this, tPathPlatformLocalData *local)
                         this->state = E_PLATFORM_ST_GET_POINT;
                     }
                 break;
-                case (enum E_ENTITY_DIR)E_PLATFORM_DIR_UP:
+                case E_PLATFORM_DIR_UP:
                     if (this->pos.y <= local->pathPos.y)
                     {
                         this->fixPos.y = itofix(local->pathPos.y);
