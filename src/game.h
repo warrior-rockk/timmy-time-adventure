1b0f5f89b18299dffaa675a5384bdbfd020680b0
warrior-rockk <warrior.rockk@gmail.com>
2026-07-13 16:21:49 +0200

    medieval map

--- src/game.h
+++ src/game.h
@@ -267,8 +267,8 @@ typedef struct
     #define DEBUG_NO_LEVEL_TIME             0
     #define DEBUG_TRACE_PLAYER              1
     #define DEBUG_LOCAL_DATA_MEMORY         0
-    #define DEBUG_INI_GAME_STATE            E_GAME_ST_TITLE// E_GAME_ST_LOAD_LEVEL
-    #define DEBUG_INI_GAME_LEVEL            E_GAME_LEVEL_EGYPT
+    #define DEBUG_INI_GAME_STATE            E_GAME_ST_LOAD_LEVEL
+    #define DEBUG_INI_GAME_LEVEL            E_GAME_LEVEL_MEDIEVAL
     typedef struct
     {
         uint8_t showDebugInfo; 
