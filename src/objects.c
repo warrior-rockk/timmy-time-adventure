6c67e31460423f1feba3a20379100e3651d155a1
warrior-rockk <warrior.rockk@gmail.com>
2026-07-13 16:26:32 +0200

    doors are 32 width

--- src/objects.c
+++ src/objects.c
@@ -249,7 +249,7 @@ void object_create(tEntity *entity)
         case E_DOOR_IN_OBJECT_TYPE:       
         case E_DOOR_OUT_OBJECT_TYPE:                    
             entity->img = NULL;
-            entity->size = (tVector){16, 32};      
+            entity->size = (tVector){32, 32};      
             entity->properties = E_ENT_PROP_NO_COLLISION;
         break;
         case E_ITEM_OBJECT_TYPE:         
