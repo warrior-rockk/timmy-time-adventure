25ffe84bc3a38f2318adf1da70ead968f44d8484
Warrior Rockk <warrior@Warrior-MBAM2.local>
2026-07-30 17:18:29 +0200

    WARNING: mod on scroll_on_position checking x equal too (problem with objects on the edge on scroll x)

--- src/scroll.c
+++ src/scroll.c
@@ -116,8 +116,8 @@ bool scroll_position_on_region(tVector checkPosition)
     //returns if the position are in the range of the scroll or not stop scroll assigned on his direction (to prevent no detection when player goes out scroll)
     return  (checkPosition.y < (scroll.pos.y + scroll.window.y) || !scroll.stopScroll.down) && 
             (checkPosition.y > (scroll.pos.y)                   || !scroll.stopScroll.up) &&
-            (checkPosition.x < (scroll.pos.x + scroll.window.x) || !scroll.stopScroll.right) && 
-            (checkPosition.x > (scroll.pos.x)                   || !scroll.stopScroll.left);
+            (checkPosition.x <= (scroll.pos.x + scroll.window.x) || !scroll.stopScroll.right) && 
+            (checkPosition.x >= (scroll.pos.x)                   || !scroll.stopScroll.left);
 }
 
 static void scroll_update_x(tVector cameraTarget, bool init)
