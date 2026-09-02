8daf479205452c288546f8053ade8575a91fc691
warrior-rockk <warrior.rockk@gmail.com>
2026-09-02 08:55:36 +0200

    fixed egypt map

--- src/game.h
+++ src/game.h
@@ -194,7 +194,7 @@
     #define DEBUG_SHOW_COLL_LAYER           2                   //value for showDebugInfo: shows only entity collision layer
     
     #define DEBUG_INI_GAME_STATE            E_GAME_ST_LOAD_LEVEL    //initial debug game state
-    #define DEBUG_INI_GAME_LEVEL            E_GAME_LEVEL_WEST   //initial debug level
+    #define DEBUG_INI_GAME_LEVEL            E_GAME_LEVEL_EGYPT   //initial debug level
 #endif
 
 //game states
